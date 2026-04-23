dbcb389b548fc159a7a52e1ba0ddbb836879cba5
Reshmi Mahadevan <reshmimahadevan04@gmail.com>
2026-04-22 17:12:23 -0700

    Adding Basic Selenium Practice Programs

---
+++ LaunchTest.java
@@ -0,0 +1,20 @@
+package seleniumsessions;
+
+public class LaunchTest {
+
+	public static void main(String[] args) {
+
+		// Before Selenium 4.6 version
+		// System.setProperty("webdriver.chrome.driver",
+		// "C:\\Users\\reshm\\OneDrive\\Desktop\\Job\\Learnings\\NaveenCourse\\Selenium\\MyDrivers\\chromedriver-win64\\chromedriver.exe");
+
+		// After Selenium 4.6 version
+		// WebDriver driver = new ChromeDriver();
+
+		// WebDriver driver = new FirefoxDrive();
+
+		// WebDriver driver = new EdgeDriver();
+
+	}
+
+}
